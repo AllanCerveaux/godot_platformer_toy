cd7875472d09105666541082188ab8d5fb6da24e
Allan <allan.cerveaux@solotech.com>
2023-10-09 00:39:30 +0200

    WIP: add wall_slide_[idle|slide|jump] and first logic step

--- src/entities/player.gd
+++ src/entities/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var debug_label: Label
 @export var raycast_top_left: RayCast2D
 @export var raycast_top_right: RayCast2D
+@export var raycast_bottom: RayCast2D
 
 @export var SPEED: float = 300.0
 @export var ACCELERATION: float = 10.0
@@ -18,13 +19,15 @@ const crouch_collision_shape: CapsuleShape2D = preload("res://src/entities/shape
 
 var DIRECTION: float = 0
 var LAST_FACING: int = 1
+var CAN_START_WALL_SLIDE: bool = true
 
 var input := Input
 var timer: Timer
+var wall_slide_timer: Timer
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var debug_text = "SPEED: {SPEED} \nVELOCITY(x,y): {X}-{Y} \nROOT SM: {ROOT_STATE} \nSUB STATE: {SUB_STATE} \n DASH_COULDOWN: {COULDOWN}/seconde"
+var debug_text = "SPEED: {SPEED} \nVELOCITY(x,y): {X}-{Y} \nROOT SM: {ROOT_STATE} \nSUB STATE: {SUB_STATE} \n DASH_COULDOWN: {COULDOWN}/sec \n WALL_SLIDE_COULDOWN: {WALL_COULDOWN}/sec"
 
 func _ready():
 	animation_tree.set_active(true)
@@ -32,20 +35,35 @@ func _ready():
 	timer = Timer.new()
 	add_child(timer)
 	timer.set_one_shot(true)
+	
+	wall_slide_timer = Timer.new()
+	add_child(wall_slide_timer)
+	wall_slide_timer.set_one_shot(true)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if not is_on_floor() and is_on_wall_only() and DIRECTION != 0:
-		velocity.y /= 2
+	if not is_on_wall() and is_on_floor() and not CAN_START_WALL_SLIDE:
+		CAN_START_WALL_SLIDE = true
 	
-	if input.is_action_just_pressed("jump") and not is_on_floor() and is_on_wall_only():
-		DIRECTION *= -1
-		velocity.y = JUMP_VELOCITY
-		velocity.x = (SPEED) * DIRECTION
+	if is_on_wall() and not raycast_bottom.is_colliding():
+		if wall_slide_timer.is_stopped() and CAN_START_WALL_SLIDE:
+			wall_slide_timer.start(1.0)
+			CAN_START_WALL_SLIDE = false
+		if input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+			if sprite.flip_h:
+				sprite.set_flip_h(false)
+				velocity.x = SPEED
+			else:
+				sprite.set_flip_h(true)
+				velocity.x = -SPEED
+		elif wall_slide_timer.is_stopped():
+			velocity.y /= 2
+		else:
+			velocity.y = 0
 
-	# Handle Jump.
 	if input.is_action_just_pressed("jump") and is_on_floor() and !input.is_action_pressed("ui_down"):
 		velocity.y = JUMP_VELOCITY
 
@@ -59,7 +77,8 @@ func _physics_process(delta: float) -> void:
 		"SPEED": SPEED,
 		"X": velocity.x,
 		"Y": velocity.y,
-		"COULDOWN": snapped(timer.time_left, 0.01)
+		"COULDOWN": snapped(timer.time_left, 0.01),
+		"WALL_COULDOWN": snapped(wall_slide_timer.time_left, 0.01)
 	})
 
 	move_and_slide()
@@ -125,6 +144,8 @@ func _on_root_state() -> void:
 				SPEED -= 5
 			
 			_handle_dash(SPEED)
+		"wall_climb_idle":
+			_update_collision_shape(default_collision_shape, 0, 16)
 		"move_machine":
 			_on_move_state()
 		"crouch_machine":
@@ -134,16 +155,16 @@ func _handle_dash(speed: float, acceleration: float = ACCELERATION) -> void:
 	velocity.x = move_toward(velocity.x, speed * LAST_FACING, acceleration)
 	
 func _handle_reset_collision_shape() -> void:
-	_update_collision_shape(default_collision_shape, 0, 23.0, 0)
+	_update_collision_shape(default_collision_shape, 0, 23.0)
 
 func switch_to_slide_shape() -> void:
 	_update_collision_shape(default_collision_shape, DIRECTION * 5, 36.0, DIRECTION * 90.0)
 
 func switch_to_crouch_shape() -> void:
-	_update_collision_shape(crouch_collision_shape, 0, 31.0, 0)
+	_update_collision_shape(crouch_collision_shape, 0, 31.0)
 
-func _update_collision_shape(shape: CapsuleShape2D, x: float, y: float, rotation_degrees: float) -> void:
+func _update_collision_shape(shape: CapsuleShape2D, x: float, y: float, degrees: float = 0) -> void:
 	collision_shape.shape = shape
 	collision_shape.position.x = x
 	collision_shape.position.y = y
-	collision_shape.rotation_degrees = rotation_degrees
+	collision_shape.rotation_degrees = degrees
